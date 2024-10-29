ce427c4b87f2b3092e717104749ce0112f78f917
hyeseong-dev <hyeseong43@gmail.com>
2024-10-29 13:04:06 +0900

    #2 - 프로젝트 셋업 및 gitignore 생성

---
+++ src/main/java/com/example/ptbatch/PtBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.ptbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtBatchApplication.class, args);
+    }
+
+}
